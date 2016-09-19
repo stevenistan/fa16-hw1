85c67b7d722658844afa643fb8e0e25cb11b3fbd
Steven Tan <steventan@berkeley.edu>
2016-09-19 14:51:30 -0700

    Finished HW1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,12 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map! {|b| b.to_i}
+    a.map! {|b| b + 2}
+    a.delete_if {|b| (b % 2) != 0}
+    a.uniq!
+    a.delete_if {|b| b > 10}
+    a.inject(0) {|sum, b| sum + b}
   end
 end
 
